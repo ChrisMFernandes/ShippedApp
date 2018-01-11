9ccd1d756f00463a05ebefba1219ececdb6ff3e3
ChrisMFernandes <Chrismfernandes12@gmail.com>
2018-01-10 22:46:38 -0500

    updated models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,7 @@
 class Job < ApplicationRecord
+	validates :name, uniqueness: true, presence: true
+	validates_numericality_of :cost, :only_integer => true, :greater_than_or_equal_to => 1000
+	validates :description, length: { minimum: 50 }
+
+	has_many :boats
 end
