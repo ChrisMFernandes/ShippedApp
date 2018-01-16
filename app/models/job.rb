c3d2c0796a32a3dbed866d766b0b7544e1116d2a
ChrisMFernandes <Chrismfernandes12@gmail.com>
2018-01-16 15:09:39 -0500

    added some ujs

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,5 +3,6 @@ class Job < ApplicationRecord
 	validates_numericality_of :cost, :only_integer => true, :greater_than_or_equal_to => 1000
 
 	has_many :boats , through: :boat_jobs
+	has_many :boat_jobs, :dependent => :destroy
 end
 
